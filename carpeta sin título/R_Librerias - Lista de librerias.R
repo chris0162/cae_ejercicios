f5eb554f19cf6d8db5f8315ec5bcb15cb5ee81bc
Christian Araya <charayasoto@gmail.com>
2024-08-20 16:04:37 -0600

    Matematica/

---
+++ carpeta sin título/R_Librerias - Lista de librerias.R
@@ -0,0 +1,7 @@
+Lista librerias
+
+library(dplyr) # parte de tidyverse para la mayoria de implementaciones
+library(plotly) # graficas en R
+library(ggplot2) # graficas en R
+library(nortest) #
+library(forecast) # predicciones de modelos de series de tiempo
